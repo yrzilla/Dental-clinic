9ae00a6a11e2fe5b185919f1e55d0626b5440121
YRZILLA <xbaracusx@mail.ru>
2022-09-26 22:15:26 +0300

    add 3 section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,11 @@
-window.addEventListener('DOMContentLoaded', () => {
 
+$(document).ready(function(){
+    $('.carousel').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next.png"></button>',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+        // dots: true
+    });
 });
